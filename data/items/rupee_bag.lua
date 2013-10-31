895aa42ab7f9d102096f64e0cdd9ef2dd3857496
Renkineko <renkineko+github@hotmail.fr>
2013-10-31 08:48:53 +0100

    Changed the rupee bag variant when entering hub, add 2 new type of purse (Fix #2)

--- data/items/rupee_bag.lua
+++ data/items/rupee_bag.lua
@@ -8,7 +8,7 @@ end
 function item:on_variant_changed(variant)
 
   -- Obtaining a rupee bag changes the max money.
-  local max_moneys = {100, 300, 999}
+  local max_moneys = {100, 300, 500, 999, 9999}
   local max_money = max_moneys[variant]
   if max_money == nil then
     error("Invalid variant '" .. variant .. "' for item 'rupee_bag'")
